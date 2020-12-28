54e6494ff321952600fce1392a8c71ceac6169da
Chanki Min <alscksrl2010@gmail.com>
2020-12-28 20:44:41 +0900

    Finish to couse: dto part 1&2

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -12,25 +14,22 @@ export class MoviesController {
     }
 
     @Get(':id')
-    getOne(@Param('id') id: string) {
+    getOne(@Param('id') id: number) {
         return this.moviesService.getOne(id);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this,this.moviesService.create(movieData);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: string) {
+    delete(@Param('id') id: number) {
         return this.moviesService.deleteOne(id);
     }
 
     @Patch(':id')
-    patch(@Param('id') id: string, @Body() updateData: any) {
-        return {
-            updatedMove : id,
-            ...updateData
-        };
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+        this.moviesService.update(id, updateData);
     }
 }
